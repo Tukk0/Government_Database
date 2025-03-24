78cd6fe231a0212d5743e594d82e3e2067447d54
Тихон Листов <listov.ta@phystech.edu>
2025-03-24 13:21:28 +0300

    Add inserts, fix ddl

--- scripts/tables_init_ddl.sql
+++ scripts/tables_init_ddl.sql
@@ -1,4 +1,4 @@
-REATE TABLE HEAD (
+CREATE TABLE HEAD (
   head_id INT PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
   title VARCHAR(64) NOT NULL,
@@ -8,6 +8,33 @@ REATE TABLE HEAD (
   valid_to DATE
 );
 
+CREATE TABLE MINISTRY (
+  ministry_id INT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  ministry_section VARCHAR(64) NOT NULL,
+  head_id INT NOT NULL UNIQUE,
+  organisation_id INT,
+  FOREIGN KEY (head_id) REFERENCES HEAD(head_id)
+);
+
+CREATE TABLE ORGANISATION (
+  organisation_id INT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  head_id INT NOT NULL UNIQUE,
+  ministry_id INT,
+  FOREIGN KEY (head_id) REFERENCES HEAD(head_id),
+  FOREIGN KEY (ministry_id) references MINISTRY(ministry_id)
+);
+
+CREATE TABLE DEPARTMENT (
+  department_id INT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  head_id INT NOT NULL UNIQUE,
+  ministry_id INT,
+  FOREIGN KEY (head_id) REFERENCES HEAD(head_id),
+  FOREIGN KEY (ministry_id) REFERENCES MINISTRY(ministry_id)
+);
+
 CREATE TABLE EMPLOYEE (
   employee_id INT PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
@@ -15,35 +42,7 @@ CREATE TABLE EMPLOYEE (
   experience INT,
   salary INT,
   valid_from DATE NOT NULL,
-  valid_to DATE
-);
-
-CREATE TABLE DEPARTMENT (
-  department_id INT PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  head_id INT NOT NULL UNIQUE,
-  employee_id INT,
-  FOREIGN KEY (head_id) REFERENCES HEAD(head_id),
-  FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
-);
-
-CREATE TABLE ORGANISATION (
-  organisation_id INT PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  head_id INT NOT NULL UNIQUE,
+  valid_to DATE,
   department_id INT,
-  FOREIGN KEY (head_id) REFERENCES HEAD(head_id),
-  FOREIGN KEY (department_id) references DEPARTMENT(department_id)
-);
-
-CREATE TABLE MINISTRY (
-  ministry_id INT PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  ministry_section VARCHAR(64) NOT NULL,
-  head_id INT NOT NULL UNIQUE,
-  organisation_id INT,
-  department_id INT,
-  FOREIGN KEY (head_id) REFERENCES HEAD(head_id),
-  FOREIGN KEY (organisation_id) REFERENCES ORGANISATION(organisation_id),
   FOREIGN KEY (department_id) REFERENCES DEPARTMENT(department_id)
-)
+);
